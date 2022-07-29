bb20d46ed8dd6a1675f14e976a48bf6284812747
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2022-07-29 03:34:09 -0500

    logic

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate() {
+    let cStart = parseInt(document.getElementById("control-start").value);
+    let cEnd = parseInt(document.getElementById("control-end").value);
+
+    let start = parseInt(document.getElementById("control-start").value);
+    let end = parseInt(document.getElementById("control-end").value);
+
+    let length;
+    let offset;
+
+    offset = calcOffset(cStart, cEnd, start, end);
+    length = calcLength(cStart, cEnd, offset);
+
+    document.getElementById("length").textContent = length;
+    document.getElementById("offset").textContent = offset;
+}
+
+function calcLength(cStart, cEnd, offset) {
+    // -($B$3*($D$3/$B$3)+D10-C10)/($D$3/$B$3)
+    return -(cStart*(cEnd/cStart)+offset-End)/(cEnd/cStart)
+}
+
+function calcOffset(cStart, cEnd, start, end) {
+    // -(((C8-B8)/(($D$3/$B$3)-1))-B8)
+    return -(((end-start)/((cEnd/cStart)-1))-start)
+}
